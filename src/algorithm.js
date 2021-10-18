bf7b36ae3787aeeee656342952d6a58613ad9e53
AlineLermen <aline.helena.lermen@gmail.com>
2021-10-18 20:23:39 -0300

    Remove arquivos desnecessários

--- src/algorithm.js
+++ src/algorithm.js
@@ -9,18 +9,14 @@ function allConstruct(word, pieces, memo = {}) {
     // se o pedaço for um prefixo da palavra
     if (word.indexOf(piece) === 0) {
       const suffix = word.slice(piece.length);
+      // chama recursivamente usando só o restante da palavra
       const suffixWays = allConstruct(suffix, pieces, memo);
       const targetWays = suffixWays.map((way) => [piece, ...way]);
       result.push(...targetWays);
-      // chama recursivamente usando só o restante da palavra
     }
   }
   memo[word] = result;
   return result;
 }
-
-console.log(
-  allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
-);
 
 export default allConstruct;
