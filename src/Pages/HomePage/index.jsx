64c7c8daced0d321e96141b35bd0a7c7e121a0f3
Arthur <arthur001123@gmail.com>
2021-10-18 20:12:47 -0300

    Adicionando palavras, trechos e integrando com o algoritmo
    Co-Authored-by: Aline Lermen <aline.helena.lermen@gmail.com>

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -2,145 +2,131 @@ import React from "react";
 import "./style.css";
 import { useHistory } from "react-router-dom";
 import Modal from "@material-ui/core/Modal";
-import Checkbox from "@material-ui/core/Checkbox";
+import allConstruct from "../../algorithm";
 
-const names = {
-  moeda1: "Moeda de 1c",
-  moeda5: "Moeda de 5c",
-  moeda10: "Moeda de 10c",
-  moeda25: "Moeda de 25c",
-  moeda50: "Moeda de 50c",
-  moeda100: "Moeda de 1r",
-  nota2: "Nota de 2",
-  nota5: "Nota de 5",
-  nota10: "Nota de 10",
-  nota20: "Nota de 20",
-  nota50: "Nota de 50",
-  nota100: "Nota de 100",
-};
+import { words, pieces } from "../../mocks";
+
+function randomizeWords(words, pieces) {
+  let drawnWord = words[Math.floor(Math.random() * words.length)];
+
+  let selectedPieces = pieces.slice(0, 8);
+
+  return { word: drawnWord, pieces: selectedPieces };
+}
 
 function HomePage() {
-  const [total, setTotal] = React.useState();
-  const [pago, setPago] = React.useState();
-  const [picks, setPicks] = React.useState({
-    moeda1: true,
-    moeda5: true,
-    moeda10: true,
-    moeda25: true,
-    moeda50: true,
-    moeda100: true,
-    nota2: true,
-    nota5: true,
-    nota10: true,
-    nota20: true,
-    nota50: true,
-    nota100: true,
-  });
+  const [guess, setGuess] = React.useState();
+  const [drawns, setDrawns] = React.useState(
+    randomizeWords(
+      words,
+      pieces.sort(() => 0.5 - Math.random())
+    )
+  );
 
   const [modal, setModal] = React.useState({
     show: false,
   });
-  const history = useHistory();
 
-    const [counter, setCounter] = React.useState(60);
-      React.useEffect(() => {
-      const timer =
-        counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-      return () => clearInterval(timer);
-    }, [counter]);
+  console.log("DRAWNS", drawns);
 
+  const [counter, setCounter] = React.useState(20);
+  React.useEffect(() => {
+    const timer =
+      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    if (counter <= 0 && !modal.show) {
+      setModal({ show: true, message: "TEMPO ESGOTADO" });
+    }
+    return () => clearInterval(timer);
+  }, [counter]);
 
   function handleClose() {
     setModal({ show: false });
   }
 
+  function restart() {
+    setCounter(20);
+    setDrawns(
+      randomizeWords(
+        words,
+        pieces.sort(() => 0.5 - Math.random())
+      )
+    );
+    setModal({
+      show: false,
+    });
+  }
+
+  console.log("CONSTRUCTIONS", modal.constructions);
+
   function handleSubmit() {
     if (
-      parseFloat(total) <= 0.0 ||
-      parseFloat(pago) <= 0.0 ||
-      parseFloat(pago) < parseFloat(total)
+      //casos de erro aqui
+      false
     ) {
       setModal({
         show: true,
+        message: "",
       });
     } else {
-      history.push({
-        pathname: "/trocado",
-        state: {
-          total: total,
-          pago: pago,
-          picks: picks,
-        },
-      });
+      let constructions = allConstruct(drawns.word, drawns.pieces);
+
+      if (guess == constructions.length) {
+        setModal({
+          show: true,
+          message: "PARABAINS",
+        });
+      } else {
+        setModal({
+          show: true,
+          message: "Tá errado",
+          constructions: constructions,
+        });
+      }
     }
   }
 
-  const handleChangeTotal = (event) => {
-    setTotal(event.target.value);
+  const handleChangeGuess = (event) => {
+    setGuess(event.target.value);
   };
-
-  const handleChangePago = (event) => {
-    setPago(event.target.value);
-  };
-
-  const handleCoinChange = (event) => {
-    let newPicks = picks;
-    newPicks[event.target.name] = event.target.checked;
-
-    setPicks(newPicks);
-  };
-
-
 
   return (
     <div className="HomePage">
       <h3 className="question">
-        {" "}
         De quantas formas a palavra pode ser formada <br />
-        utilizando os trechos mostrados? <br /><br /><br />
-        Timer: {counter}
+        utilizando os trechos mostrados? <br />
+        <br />
       </h3>
+      <div className="word"> Palavra: {drawns.word} </div>
+      <div className="piecesArea">
+        {drawns.pieces.map((piece) => {
+          return (
+            <div className="piece">
+              {piece} <br></br>
+            </div>
+          );
+        })}
+      </div>
       <br />
       <br />
-      <br />
-
       <div>
         <label className="label">
           É possível formar a palavra de <nbsp />
           <input
             className="textField"
             type="number"
-            value={total}
+            value={guess}
             step="1"
-            onChange={handleChangeTotal}
+            onChange={handleChangeGuess}
           />
           <nbsp /> formas diferentes
-          <br />
-          <br />
-    
-          <div className="pickArea">
-            {Object.entries(picks).map((key, value) => {
-              return (
-                <div key={key[0]} className="checkDiv">
-                  {names[key[0]]}{" "}
-                  <Checkbox
-                    className="checkbox"
-                    type="checkbox"
-                    value={picks[key[0]]}
-                    name={key[0]}
-                    onChange={handleCoinChange}
-                    defaultChecked={true}
-                  />
-                </div>
-              );
-            })}
-          </div>
           <br />
         </label>
         <button className="calculateButton" onClick={handleSubmit}>
           {" "}
           Responder
         </button>
+        <br />
+        {counter} Segundos restantes
       </div>
 
       <Modal
@@ -155,11 +141,20 @@ function HomePage() {
         }}
       >
         <div className="modaldiv">
-          <h1>O valor pago precisa ser maior que o valor total</h1>
-
-          <button className="calculateButton" onClick={handleClose}>
+          <h1 className="modalMessage">{modal.message}</h1>
+          {modal.constructions ? <>Possibilidades:</> : <></>}
+          {modal.constructions?.map((construction) => {
+            return (
+              <div className="construction">
+                {construction.map((piece) => {
+                  return <div className="constructionPiece">{piece} </div>;
+                })}{" "}
+              </div>
+            );
+          })}
+          <button className="calculateButton" onClick={restart}>
             {" "}
-            Voltar
+            Reiniciar
           </button>
         </div>
       </Modal>
---
+++ src/mocks.js
@@ -0,0 +1,30 @@
+const words = [
+  "abra",
+  "braba",
+  "bar",
+  "barca",
+  "aba",
+  "cara",
+  "acaba",
+  "rara",
+  "arara",
+  "arca",
+];
+
+const pieces = [
+  "a",
+  "rc",
+  "b",
+  "c",
+  "r",
+  "ar",
+  "br",
+  "abr",
+  "bra",
+  "ca",
+  "ba",
+  "ab",
+  "ra",
+];
+
+export { words, pieces };
