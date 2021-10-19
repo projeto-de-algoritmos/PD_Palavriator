ff10fa1d64824a21403df5da59c30061584be807
AlineLermen <aline.helena.lermen@gmail.com>
2021-10-19 13:25:29 -0300

    Remove linha que quebrava o programa

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
-import Trocado from "../Pages/Trocado";
 
 const Router = () => {
   return (
     <Switch>
       <Route path="/" exact component={HomePage} />
-      <Route path="/trocado" exact component={Trocado} />
 
       <Redirect path="*" to="/" />
     </Switch>
